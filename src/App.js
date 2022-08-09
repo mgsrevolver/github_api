9624bc28bce1eea918a93c21a3869728ca0c1c2a
mgsrevolver <clayreimus@gmail.com>
2022-08-09 15:02:19 -0400

    messing with tables
    trying to coerce data into a table that works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg'
 import { useEffect, useState } from 'react'
 import './App.css'
 import Card from 'react-bootstrap/Card'
@@ -11,6 +10,7 @@ function App() {
   const APIkey = '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'
   const allModsURL =
     'https://api.github.com/repos/roll20/roll20-api-scripts/git/trees/master'
+  const profileURL = 'https://api.github.com/users/Roll20'
 
   async function repoDataURL() {
     //Get repo data about all mods in github
@@ -25,11 +25,14 @@ function App() {
         (result) => {
           console.log(36, result.tree)
           const list = result.tree.map((item) => (
-            <div className="text-center">
-              <a target="_blank" href={item.url}>
-                {item.path}
-              </a>
-            </div>
+            <tr>
+              <td>
+                <a target="_blank" href={item.url}>
+                  {item.path}
+                </a>
+              </td>
+              <td>{item.sha}</td>
+            </tr>
           ))
           setRepoData(list)
         },
@@ -39,20 +42,6 @@ function App() {
       )
   }
 
-  useEffect(() => {
-    fetch('https://api.github.com/users/Roll20')
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          console.log(result)
-          setAvatarURL(result.avatar_url)
-          setGitHubUsername(result.login)
-        },
-        (error) => {
-          console.log(error)
-        }
-      )
-  }, [])
   return (
     <div className="App w-100 min-vh-100 justify-content-center align-items-center d-flex flex-column">
       <Card style={{ width: '18rem' }}>
@@ -65,7 +54,15 @@ function App() {
           </Button>
         </Card.Body>
       </Card>
-      {repoData}
+      <table>
+        <thread>
+          <tr>
+            <th>Mod Name</th>
+            <th>sha</th>
+          </tr>
+        </thread>
+        <tbody>{repoData}</tbody>
+      </table>
     </div>
   )
 }
---
+++ src/Table.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {} from './App'
+
+function JsonDataDisplay() {
+  const DisplayData = JsonData.map((info) => {
+    return (
+      <tr>
+        <td>{info.id}</td>
+        <td>{info.name}</td>
+        <td>{info.city}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <div>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Sr.NO</th>
+            <th>Name</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>{DisplayData}</tbody>
+      </table>
+    </div>
+  )
+}
+
+export default JsonDataDisplay
