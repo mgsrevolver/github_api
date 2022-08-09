76cf5a2f0900de03edd756ff6194da0b0a23bab9
mgsrevolver <clayreimus@gmail.com>
2022-08-09 13:11:30 -0400

    configured to fetch list of all trees in github master!
    now need to add credentials to avoid rate limiting

--- src/App.js
+++ src/App.js
@@ -1,53 +1,57 @@
-import logo from "./logo.svg";
-import { useEffect, useState } from "react";
-import "./App.css";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import logo from './logo.svg'
+import { useEffect, useState } from 'react'
+import './App.css'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 function App() {
-  const [avatarURL, setAvatarURL] = useState();
-  const [githubUsername, setGitHubUsername] = useState();
-  const [repoData, setRepoData] = useState();
+  const [avatarURL, setAvatarURL] = useState()
+  const [githubUsername, setGitHubUsername] = useState()
+  const [repoData, setRepoData] = useState()
 
   async function repoDataURL() {
     //Get repo data about github user
-    await fetch("https://api.github.com/users/LiveDetermined/repos")
+    await fetch(
+      'https://api.github.com/repos/roll20/roll20-api-scripts/git/trees/master'
+    )
+      // https://api.github.com/users/Roll20/repos
+      // https://api.github.com/repos/roll20/roll20-api-scripts/git/trees/master
       .then((res) => res.json())
       .then(
         (result) => {
-          console.log(36, result);
-          const list = result.map((item) => (
+          console.log(36, result.tree)
+          const list = result.tree.map((item) => (
             <div className="text-center">
-              <a target="_blank" href={item.svn_url}>
-                {item.name}
+              <a target="_blank" href={item.url}>
+                {item.path}
               </a>
             </div>
-          ));
-          setRepoData(list);
+          ))
+          setRepoData(list)
         },
         (error) => {
-          console.log(error);
+          console.log(error)
         }
-      );
+      )
   }
 
   useEffect(() => {
-    fetch("https://api.github.com/users/Livedetermined")
+    fetch('https://api.github.com/users/Roll20')
       .then((res) => res.json())
       .then(
         (result) => {
-          console.log(result);
-          setAvatarURL(result.avatar_url);
-          setGitHubUsername(result.login);
+          console.log(result)
+          setAvatarURL(result.avatar_url)
+          setGitHubUsername(result.login)
         },
         (error) => {
-          console.log(error);
+          console.log(error)
         }
-      );
-  }, []);
+      )
+  }, [])
   return (
     <div className="App w-100 min-vh-100 justify-content-center align-items-center d-flex flex-column">
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={avatarURL} />
         <Card.Body>
           <Card.Title>{githubUsername}</Card.Title>
@@ -59,7 +63,7 @@ function App() {
       </Card>
       {repoData}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
